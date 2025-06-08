f6a54d9513f998f6d2185e073b73831495c92636
JakubGodlewski <jakubgodlewski2003@gmail.com>
2025-06-08 17:57:39 +0200

    sympatyczny front

--- letterbox/src/app/app.component.ts
+++ letterbox/src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BodyComponent } from './body/body.component';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BodyComponent, HeaderComponent],
+  imports: [BodyComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   leftIsShown = false;
+  footerVisible = false;
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const windowHeight = window.innerHeight;
+    const docHeight = document.documentElement.scrollHeight;
+
+    this.footerVisible = scrollTop + windowHeight >= docHeight/2;
+    console.log("konieccc");
+  }
 
   onShowLeft(event: boolean) {
     this.leftIsShown = event;
--- letterbox/src/app/body/body.component.ts
+++ letterbox/src/app/body/body.component.ts
@@ -1,11 +1,12 @@
 import {Component, Input, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MovieComponent} from './movie/movie.component';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, MovieComponent]
 })
 export class BodyComponent {
   @Input() leftIsShown = false;
